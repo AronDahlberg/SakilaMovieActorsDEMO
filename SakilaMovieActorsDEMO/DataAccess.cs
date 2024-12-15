03c4e81522718799896c2737c2d995143bdb0ae3
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-12-15 20:02:15 +0100

    Implemented connection string in dataAccess

--- SakilaMovieActorsDEMO/DataAccess.cs
+++ SakilaMovieActorsDEMO/DataAccess.cs
@@ -1,7 +1,19 @@
-﻿namespace SakilaMovieActorsDEMO
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SakilaMovieActorsDEMO
 {
     internal class DataAccess
     {
+        private readonly string? connectionString;
+
+        public DataAccess()
+        {
+            connectionString = new ConfigurationBuilder()
+                               .AddJsonFile("appsettings.json")
+                               .Build()
+                               .GetConnectionString("Sakila");
+        }
+
         public IEnumerable<string> GetActorMovies(string actorName)
         {
             throw new NotImplementedException();
