5d115a3a164c4e482313d0b01dd33e7acee0d4ef
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-12-15 12:46:30 +0100

    Add project files.

---
+++ SakilaMovieActorsDEMO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SakilaMovieActorsDEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
